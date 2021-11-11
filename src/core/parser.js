bb740b3eacea3dc16094b5b6183479523844f1c6
sakkke <w32w64@gmail.com>
2021-11-11 21:00:19 +0900

    add parse method

--- src/core/parser.js
+++ src/core/parser.js
@@ -9,4 +9,27 @@ export default class {
       .flatMap(token => token.split(' '))
     return tokens
   }
+
+  parse () {
+    function createResult (value, type) {
+      const result = { type, value }
+      return result
+    }
+
+    const result = this.tokens.map(token => {
+      switch (true) {
+        case /\d+/.test(token):
+          const number = parseInt(token, 10)
+          return createResult(number, 'number')
+
+        case /^:/.test(token):
+          const operatorName = token.slice(1)
+          return createResult(operatorName, 'operator')
+
+        default:
+          return createResult(token, 'string')
+      }
+    })
+    return result
+  }
 }
