2856c0d026b304b961df03d95a8e28048e87db7b
sakkke <w32w64@gmail.com>
2021-11-13 09:44:04 +0900

    update operator class

--- src/core/operator.js
+++ src/core/operator.js
@@ -1,14 +1,11 @@
 export default class {
-  static argN = 1
+  static argN = 0
 
   constructor (args) {
     this.args = args
   }
 
   body () {
-    return {
-      type: 'string',
-      value: this.args[0],
-    }
+    return undefined
   }
 }
