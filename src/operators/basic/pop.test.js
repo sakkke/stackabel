9274ba536072b824c0c4406f5e701cde9f6f36fa
sakkke <w32w64@gmail.com>
2021-11-16 06:54:51 +0900

    :test_tube: add test

--- src/operators/basic/pop.test.js
+++ src/operators/basic/pop.test.js
@@ -7,6 +7,16 @@ test('1 :push :pop', () => {
   expect(parser.stack).toEqual([{ type: 'number', value: 1 }])
 })
 
+test('1 2 3 :push :push :push :pop :pop :pop', () => {
+  const parser = new Parser('1 2 3 :push :push :push :pop :pop :pop')
+  parser.eval()
+  expect(parser.stack).toEqual([
+    { type: 'number', value: 1 },
+    { type: 'number', value: 2 },
+    { type: 'number', value: 3 },
+  ])
+})
+
 test('operator', () => {
   const operator = new PopOperator()
   operator.substack.push({ type: 'number', value: 1 })
